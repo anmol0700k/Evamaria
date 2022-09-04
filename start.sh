33b9ecc0a3e39993ca6ee25a2dd3c0000d1b5d96
Anmolkanchru <sonu440579@gmail.com>
2022-09-04 08:48:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/MrMKN/PROFESSOR-BOT.git /PROFESSOR-BOT
+  git clone https://github.com/Anmolkanchru/Captain-v2.git /Captain-v2
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /PROFESSOR-BOT
+  git clone $UPSTREAM_REPO /Captain-v2
 fi
-cd /PROFESSOR-BOT
+cd /Captain-v2
 pip3 install -U -r requirements.txt
 echo "Starting 𝙼𝙺𝙽 𝙱𝙾𝚃𝚉....🔥🔥"
 python3 bot.py
